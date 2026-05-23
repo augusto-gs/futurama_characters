88a07e461c7dc6632e08c8be126cf41593ad3a3f
Augusto Gomez <agomezsantos95@gmail.com>
2026-05-23 13:49:51 +0200

    Add types for characters

---
+++ src/types/characters.ts
@@ -0,0 +1,22 @@
+export interface CharacterName {
+  first: string;
+  second: string;
+  last: string;
+}
+
+export interface CharacterImages {
+  "head-shot": string;
+  main: string;
+}
+
+export interface Character {
+  id: number;
+  name: CharacterName;
+  images: CharacterImages;
+  gender: string;
+  species: string;
+  homePlanet: string;
+  occupation: string;
+  age?: string;
+  sayings?: string[];
+}
